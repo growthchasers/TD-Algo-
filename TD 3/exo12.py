50bbecc6fce0081134da37c5c24c6cc25ca4a86c
yamilobt8 <yamilo12345678910@gmail.com>
2024-11-19 16:05:59 +0100

    exo12 - 13

---
+++ TD 3/exo12.py
@@ -0,0 +1,23 @@
+"""
+1
+11
+121
+1331
+14641
+"""
+n = int(input('entrez la valuer de N: '))
+
+pascal_triangle = [[1]] # Triangle Base
+
+for i in range(n):
+    pascal_row = [1] # row Edges
+    for j in range(len(pascal_triangle[i]) - 1):
+        # Add adjacent elements From the previous row
+        pascal_row.append(pascal_triangle[i][j] + pascal_triangle[i][j + 1]) 
+
+    pascal_row.append(1) # row Edges
+    pascal_triangle.append(pascal_row)
+
+for row in pascal_triangle:
+    print(row)
+
---
+++ TD 3/exo13.py
@@ -0,0 +1,5 @@
+s = 1
+for i in range(10, 21):
+    print(i)
+    s += 1 / i
+print(f'S = {s}')
